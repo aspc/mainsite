8cd4f47c4f5bb711c385c39ffcecb8aa000d7be9
Joseph Long <josephoenix@gmail.com>
2011-09-01 10:34:11 -0700

    Added urlpattern for info

--- aspc/urls.py
+++ aspc/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from aspc.folio.models import Page
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    
+    url(r'information/(?P<slug_path>(?:[\w\-\d]+/)+)$', 'aspc.folio.views.page_view', {'section': Page.SECTION_LOOKUP['Information'],})
     # Examples:
     # url(r'^$', 'aspc.views.home', name='home'),
     # url(r'^aspc/', include('aspc.foo.urls')),
