5d458886acef86e21525018147f223cb737883ac
Joseph Long <josephoenix@gmail.com>
2011-08-31 19:15:09 -0700

    enabling admin urls

--- aspc/urls.py
+++ aspc/urls.py
@@ -1,17 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    
     # Examples:
     # url(r'^$', 'aspc.views.home', name='home'),
     # url(r'^aspc/', include('aspc.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
