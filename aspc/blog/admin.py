793caf29c0ca33a8e5787db8991d1dfefe74ef74
Joseph Long <josephoenix@gmail.com>
2011-09-15 20:32:38 -0700

    prepopulate slug and make pretty list display

--- aspc/blog/admin.py
+++ aspc/blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from aspc.blog.models import Post
 
+class PageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "slug", "author", "posted")
+
 admin.site.register(Post)
