c046346c787e315227f9d9164e807c7542c1c07a
Joseph Long <josephoenix@gmail.com>
2011-09-26 23:44:39 -0700

    Reworked the room detail page header

--- aspc/housing/templatetags/room_tools.py
+++ aspc/housing/templatetags/room_tools.py
@@ -5,7 +5,10 @@ register = template.Library()
 
 @register.inclusion_tag('housing/stars.html')
 def show_stars(num):
-    return {'stars': '&#10025;&nbsp;'*int(num+1), 'number': int(num+1),}
+    if num:
+        return {'stars': '&#9733;&nbsp;'*int(num+1), 'number': int(num+1),}
+    else:
+        return {}
 
 @register.inclusion_tag('housing/suite_snippet.html')
 def suite(room):
