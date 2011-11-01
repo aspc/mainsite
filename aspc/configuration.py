383c0d91fb8d56c9f2c0efe592d032ea3c25e1e7
Joseph Long <josephoenix@gmail.com>
2011-10-31 21:44:06 -0700

    Adding a debugging log level

--- aspc/configuration.py
+++ aspc/configuration.py
@@ -108,11 +108,24 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler'
         }
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
     },
     'loggers': {
         'django.request': {
@@ -120,6 +133,10 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
+        'aspc': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        }
     }
 }
 
