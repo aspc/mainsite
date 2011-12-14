a60ce38182e3fad7fe9a3b71b546e2e09efb8297
Joseph Long <josephoenix@gmail.com>
2011-12-14 15:07:26 -0800

    whoops rogue import

--- aspc/minutes/admin.py
+++ aspc/minutes/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.db import models
 from aspc.minutes.models import MeetingMinutes
-from aspc.widgets import MarkdownTextarea
 
 class MeetingMinutesAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
