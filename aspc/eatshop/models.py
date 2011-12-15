4c98f62f151a3a49c08a3004723ad78e4b423873
Joseph Long <josephoenix@gmail.com>
2011-12-14 21:13:10 -0800

    starting work on eatshop

---
+++ aspc/eatshop/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.localflavor.us.models import PhoneNumberField
+
+class BusinessManager(models.Manager):
+    def off_campus(self):
+        return self.get_query_set().exclude(type=Business.TYPES_LOOKUP['On-Campus Restaurant'])
+    
+    def on_campus(self):
+        return self.get_query_set().filter(type=Business.TYPES_LOOKUP['On-Campus Restaurant'])
+
+class Business(models.Model):
+    """A local/on-campus business"""
+    TYPES = (
+        (0, "On-Campus Restaurant"),
+        (1, "Restaurant"),
+        (2, "Snacks & Treats"),
+        (3, "Beauty and Health"),
+        (4, "Apparel"),
+        (5, "Groceries"),
+        (6, "Other"),
+    )
+    TYPES_LOOKUP = dict([(b,a) for a,b in TYPES])
+    
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone = PhoneNumberField(null=True, blank=True)
+    discount = models.TextField(null=True, blank=True)
+    www = models.URLField(null=True, blank=True)
+    
+    objects = BusinessManager()
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name, verbose_name_plural = "business", "businesses"
+
+    def __unicode__(self):
+        return u"{0}".format(self.name)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('business_detail', [self.id])
+
+
+class Hours(models.Model):
+    business = models.ForeignKey(Business)
+    monday = models.BooleanField()
+    tuesday = models.BooleanField()
+    wednesday = models.BooleanField()
+    thursday = models.BooleanField()
+    friday = models.BooleanField()
+    begin = models.TimeField()
+    end = models.TimeField()
+
+    def gen_days(self):
+        s = []
+        if self.monday: s.append('M')
+        if self.tuesday: s.append('T')
+        if self.wednesday: s.append('W')
+        if self.thursday: s.append('R')
+        if self.friday: s.append('F')
+        return s
+
+    def __unicode__(self):
+        return u'[%s] Open %s, %s-%s' % (self.business.name, ''.join(self.gen_days()), self.begin.strftime('%I:%M %p'), self.end.strftime('%I:%M %p'))
