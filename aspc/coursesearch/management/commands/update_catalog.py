638b21084336522a61908f7a2684f9fed3e7118f
Joseph Long <josephoenix@gmail.com>
2012-03-31 12:45:11 -0700

    name collision

--- aspc/coursesearch/management/commands/update_catalog.py
+++ aspc/coursesearch/management/commands/update_catalog.py
@@ -2,7 +2,7 @@ import logging
 import pyodbc
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-from aspc.coursesearch.tasks import update_catalog
+from aspc.coursesearch.tasks import update_catalog as update_task
 
 logger = logging.getLogger(__name__)
 
@@ -13,6 +13,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         logger.info("Starting full catalog update")
         logger.info("Connecting to {0}".format(settings.COURSE_DATA_DB['HOST']))
-        update_catalog.delay()
-        update_catalog.wait()
+        update_task.delay()
+        update_task.wait()
         logger.info("Full catalog update finished")
