628c302453678dc9cf7751f723f7f7e2d73e9d81
Joseph Long <josephoenix@gmail.com>
2012-04-02 19:04:44 -0700

    Cosmetic improvements and the addition of an 'only open courses' filter and requirementarea filter

--- aspc/coursesearch/models.py
+++ aspc/coursesearch/models.py
@@ -4,7 +4,8 @@ from datetime import date, time, datetime, timedelta
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 
-CAMPUSES = ((1, u'PO'), (2, u'SC'), (3, u'CM'), (4, u'HM'), (5, u'PZ'), (6, u'CGU'), (7, u'CU'), (-1, u'?'))
+CAMPUSES = ((1, u'PO'), (2, u'SC'), (3, u'CMC'), (4, u'HM'), (5, u'PZ'), (6, u'CGU'), (7, u'CU'), (-1, u'?'))
+CAMPUSES_FULL_NAMES = {1: 'Pomona', 2: 'Scripps', 3: 'Claremont-McKenna', 4: 'Harvey Mudd', 5: 'Pitzer'}
 CAMPUSES_LOOKUP = dict([(a[1], a[0]) for a in CAMPUSES])
 START_DATE = date(2012, 9, 3)
 
