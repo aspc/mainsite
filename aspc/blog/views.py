40dc0665008dab783f5fcbf61282ab4ecb1e3395
Joseph Long <josephoenix@gmail.com>
2012-09-02 15:12:20 -0700

    Factored the fix for month archives into a generic view (aspc.generic.FilteredMonthArchiveView) and fixed meeting minutes archive

--- aspc/blog/views.py
+++ aspc/blog/views.py
@@ -1,43 +1,15 @@
 from django.views.generic.dates import (DateDetailView, ArchiveIndexView, 
     MonthArchiveView, _date_from_string)
 from django.http import Http404
+from aspc.generic import FilteredMonthArchiveView
 import datetime
 
 class PostDetail(DateDetailView):
     def get_object(self):
         return self.model.objects.get(slug=self.kwargs['slug'].lower())
 
-class PostMonthArchive(MonthArchiveView):
-    def _previous_posts(self):
-        year, month, day = self.get_year(), self.get_month(), '1'
-        format = '{0}-{1}-{2}'.format(
-            self.get_year_format(),
-            self.get_month_format(),
-            '%d'
-        )
-        timestr = '{0}-{1}-{2}'.format(year, month, day)
-        month_start = datetime.datetime.strptime(timestr, format)
-        
-        return self.model.objects.filter(**{
-            '{0}__lt'.format(self.date_field): month_start,
-        })
-    
-    def get_context_data(self, **kwargs):
-        context = super(PostMonthArchive, self).get_context_data(**kwargs)
-        
-        current_exists = context[self.get_context_object_name(self.model)].exists()
-        previous_exists = self._previous_posts().exists()
-        
-        if (not current_exists) and (not previous_exists):
-            raise Http404 # Nothing in this month, nothing prior to it,
-                          # better bail
-        elif not previous_exists:
-            context['previous_posts_exist'] = False
-        else:
-            context['previous_posts_exist'] = True
-        
-        return context
-
+class PostMonthArchive(FilteredMonthArchiveView):
+    pass
 
 class PostArchive(ArchiveIndexView):
     def get_context_data(self, **kwargs):
