a8d107ae825543c1ebecc44504951e0945775123
Joseph Long <josephoenix@gmail.com>
2012-09-17 20:25:20 -0700

    Added django-grappelli dependency

--- aspc/configuration.py
+++ aspc/configuration.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'grappelli', # Must be before django.contrib.admin
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.markup',
