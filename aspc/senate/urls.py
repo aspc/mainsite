d698d4ce3002db3b518e061075f294cf9b0089a6
Joseph Long <josephoenix@gmail.com>
2012-10-10 10:47:18 -0700

    Remove the preview prefix from the positions URL pattern

--- aspc/senate/urls.py
+++ aspc/senate/urls.py
@@ -4,5 +4,5 @@ from aspc.senate.views import DocumentList, AppointmentList
 urlpatterns = patterns('',
     url(r'^documents/$', DocumentList.as_view(), name="document_list"),
     url(r'^documents/(?P<page>[0-9]+)/$', DocumentList.as_view(), name="document_list_page"),
-    url(r'^preview/positions/$', AppointmentList.as_view(), name="positions"),
+    url(r'^positions/$', AppointmentList.as_view(), name="positions"),
 )
