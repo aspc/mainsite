5c996329704e745eeb8c0a86dfef4e1959ec9667
Joseph Long <josephoenix@gmail.com>
2013-08-26 23:52:37 -0400

    change schedule share view to support absolute urls with ports for development

--- aspc/context_processors.py
+++ aspc/context_processors.py
@@ -1,7 +1,10 @@
 from django.contrib.sites.models import Site, RequestSite
 
 def site(request):
-    site_info = {'protocol': request.is_secure() and 'https' or 'http'}
+    site_info = {
+        'protocol': request.is_secure() and 'https' or 'http',
+        'port': request.META.get('SERVER_PORT')
+    }
     if Site._meta.installed:
         site_info['domain'] = Site.objects.get_current().domain
     else:
