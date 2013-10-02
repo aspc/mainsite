934c696fc106d77b76916d2a47d9492317c05888
Matt Dahl <matt.dahl.2013@gmail.com>
2013-10-01 23:34:48 -0700

    Overrides Hours#save method to invalidate the cache when the data is updated.

--- aspc/eatshop/models.py
+++ aspc/eatshop/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.localflavor.us.models import PhoneNumberField
 import datetime
 from aspc.eatshop.config import COOP_FOUNTAIN_ID
+import hashlib
+from django.utils.http import urlquote
+from django.core.cache import cache
 
 class BusinessManager(models.Manager):
     def off_campus(self, qs=None):
@@ -10,14 +13,14 @@ class BusinessManager(models.Manager):
         return qs.exclude(
             type=Business.TYPES_LOOKUP['On-Campus Restaurant']
         )
-    
+
     def restaurants(self, qs=None):
         """Off-campus restaurants only"""
         qs = qs or self.get_query_set()
         return qs.filter(
             type=Business.TYPES_LOOKUP['Restaurant']
         )
-    
+
     def non_food(self, qs=None):
         """Only businesses that aren't restaurants"""
         qs = qs or self.get_query_set()
@@ -27,14 +30,14 @@ class BusinessManager(models.Manager):
                 Business.TYPES_LOOKUP['On-Campus Restaurant'],
             ]
         )
-    
+
     def on_campus(self, qs=None):
         """Only on-campus restaurants"""
         qs = qs or self.get_query_set()
         return qs.filter(
             type=Business.TYPES_LOOKUP['On-Campus Restaurant']
         )
-    
+
     def open_now(self, qs=None):
         """
         All businesses open now (aka is the current time within
@@ -61,7 +64,7 @@ class Business(models.Model):
         (6, "Other"),
     )
     TYPES_LOOKUP = dict([(b,a) for a,b in TYPES])
-    
+
     name = models.CharField(max_length=255)
     type = models.IntegerField(choices=TYPES)
     address = models.TextField()
@@ -71,9 +74,9 @@ class Business(models.Model):
     flex = models.BooleanField()
     discount = models.TextField(null=True, blank=True, verbose_name="student discount")
     www = models.URLField(null=True, blank=True)
-    
+
     objects = BusinessManager()
-    
+
     class Meta:
         ordering = ['name']
         verbose_name, verbose_name_plural = "business", "businesses"
@@ -91,14 +94,14 @@ class Business(models.Model):
             return ('restaurants', [self.id])
         else:
             return ('businesses', [self.id])
-    
+
     @property
     def is_open(self):
         if self.objects.open_now().filter(pk=self.pk).count():
             return True
         else:
             return False
-    
+
     def has_discount(self):
         """Does this business have student discount information?"""
         return bool(self.discount)
@@ -116,11 +119,11 @@ class Hours(models.Model):
     sunday = models.BooleanField()
     begin = models.TimeField()
     end = models.TimeField()
-    
+
     class Meta:
         ordering = ['begin', 'end']
         verbose_name, verbose_name_plural = "hours", "hours"
-    
+
     def gen_days(self):
         s = []
         if self.monday: s.append('M')
@@ -139,3 +142,22 @@ class Hours(models.Model):
             self.begin.strftime('%I:%M %p'),
             self.end.strftime('%I:%M %p')
         )
+
+    # Override this method to invalidate cached business template for the business that is being updated (saved)
+    def save(self, *args, **kwargs):
+        # Code extracted from django version 1.6: https://github.com/django/django/blob/master/django/core/cache/utils.py
+        # When we upgrade to that version we can simply do "from django.core.cache import cache" and invoke this method
+
+        TEMPLATE_FRAGMENT_KEY_TEMPLATE = 'template.cache.%s.%s'
+        def make_template_fragment_key(fragment_name, vary_on=None):
+            if vary_on is None:
+                vary_on = ()
+            key = ':'.join(urlquote(var) for var in vary_on)
+            args = hashlib.md5(key)
+            return TEMPLATE_FRAGMENT_KEY_TEMPLATE % (fragment_name, args.hexdigest())
+
+        # Generates the appropriate key (a md5 hash, not just a string) and deletes it from the cache
+        key = make_template_fragment_key('business_info_fragment', [self.business])
+        cache.delete(key)
+
+        super(Hours, self).save(*args, **kwargs)
