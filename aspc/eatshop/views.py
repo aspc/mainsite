a1490c1fc0b54de42af05ae99d8b6d30f4e03bd1
Matt Dahl <matt.dahl.2013@gmail.com>
2013-10-01 22:13:42 -0700

    Switches to template fragment caching that offers more finesse.

--- aspc/eatshop/views.py
+++ aspc/eatshop/views.py
@@ -23,23 +23,18 @@ def coop_fountain(request):
     return render(request, "eatshop/coop_fountain.html",
                   {'business': coop, 'page': page})
 
-# Cache these pages for 24 hours
-@cache_page(60 * 60 * 24)
 def on_campus(request):
     f = OnCampusFilterSet(request.GET, queryset=Business.objects.on_campus())
     return render(request, 'eatshop/on_campus_list.html', {'filter': f})
 
-@cache_page(60 * 60 * 24)
 def restaurants(request):
     f = RestaurantsFilterSet(request.GET, queryset=Business.objects.restaurants())
     return render(request, 'eatshop/restaurants_list.html', {'filter': f})
 
-@cache_page(60 * 60 * 24)
 def businesses(request):
     f = AllBusinessesFilterSet(request.GET, queryset=Business.objects.non_food())
     return render(request, 'eatshop/businesses_list.html', {'filter': f})
 
-@cache_page(60 * 60 * 24)
 def home(request):
     f = AllBusinessesFilterSet(request.GET, queryset=Business.objects.all())
     return render(request, 'eatshop/all_businesses_list.html', {'filter': f})
