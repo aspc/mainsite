9b434ccf1d62de308d919758052c7596f84605f9
Matt Dahl <matt.dahl.2013@gmail.com>
2013-10-12 14:56:39 -0700

    Adds Event model.

--- aspc/events/models.py
+++ aspc/events/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
-# Calendar
+CHARFIELD_MAX_LENGTH = 255
 
-# Event
+class Event(models.Model):
+    name = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    location = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
+    description = models.TextField()
+    status = models.CharField(max_length=CHARFIELD_MAX_LENGTH) #  pending, approved, or denied
 
-# MainCalendar
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('start', 'name', 'end')
+        verbose_name_plural = "published events"
