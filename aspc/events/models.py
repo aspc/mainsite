13d1a4aefb84a254d3ec1fc6923793a9fe65579d
Matt Dahl <matt.dahl.2013@gmail.com>
2013-10-16 17:24:04 -0700

    Defines an EventController class to handle the logic of managing events.

--- aspc/events/models.py
+++ aspc/events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from aspc.events.backends.facebook import FacebookBackend
 
 CHARFIELD_MAX_LENGTH = 255
 
@@ -16,3 +17,25 @@ class Event(models.Model):
     class Meta:
         ordering = ('start', 'name', 'end')
         verbose_name_plural = "published events"
+
+class EventController(object):
+	def __unicode__(self):
+	    return self.name
+
+	@staticmethod
+	def new_event(data):
+		fb = FacebookBackend()
+		if data['event_source'] == 'facebook':
+		    event_data = fb.get_event_data(data['event_url'])
+
+		    event = Event()
+
+		    for key, value in event_data.items():
+		        setattr(event, key, value)
+
+		    # Creates a new Event model with the Facebook data
+		    event.save()
+
+		    return event
+		else:
+			return False
