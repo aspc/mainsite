26ed0fd13b0230d6ea4046b287c2f5605cadcf5f
Matt Dahl <matt.dahl.2013@gmail.com>
2013-10-31 21:15:36 -0700

    Switches todays_events template to render dynamic data from database.

--- aspc/static/js/home.js
+++ aspc/static/js/home.js
@@ -11,7 +11,7 @@ ASPC.Home.update_event_description = function (event) {
 	// Declares a reference to the event_info div that was clicked
 	var event_info = $(event.currentTarget).children('aside');
 
-	// Updates teh events_description panel with the appropriate information
+	// Updates the events_description panel with the appropriate information
 	$('#events_description_title').html(event_info.children('h3').html());
 	$('#events_description_host').html(event_info.children('p.host').html());
 	$('#events_description_location').html(event_info.children('p.location').html());
@@ -25,5 +25,12 @@ ASPC.Home.update_event_description = function (event) {
 };
 
 window.onload = function () {
+	// Initializes the events_descriptions panel
+	var first_event = $('li.active').find('aside');
+	$('#events_description_title').html(first_event.children('h3').html());
+	$('#events_description_host').html(first_event.children('p.host').html());
+	$('#events_description_location').html(first_event.children('p.location').html());
+	$('#events_description_description').html(first_event.children('p.description').html());
+
 	$(".event_info").on('click', ASPC.Home.update_event_description);
 };
