8b9653f1b6f0a3f0514e1e631b50df2ea112f272
Matt Dahl <matt.dahl.2013@gmail.com>
2013-11-11 23:38:03 -0800

    Adds frontend JS handling for POSTing a manual event to the server.

--- aspc/static/js/events.js
+++ aspc/static/js/events.js
@@ -42,3 +42,36 @@ ASPC.Events.submit_facebook_event = function () {
 		}
 	});
 };
+
+ASPC.Events.submit_manual_event = function () {
+	var manual_event = {
+		event_source: 'manual',
+		event_name: $('#manual_event_name').val(),
+		event_start: $('#manual_event_start').val(),
+		event_end: $('#manual_event_end').val(),
+		event_location: $('#manual_event_location').val(),
+		event_description: $('#manual_event_description').val(),
+		event_host: $('#manual_event_host').val(),
+		event_url: $('#manual_event_url').val()
+	};
+
+	$.ajax({
+		type: 'POST',
+		beforeSend: function (request) {
+			request.setRequestHeader("X-CSRFToken", ASPC.csrf_token);
+		},
+		data: manual_event,
+		timeout: 10000,
+		success: function (data) {
+			console.log('success');
+			new_event = JSON.parse(data)[0].fields;
+			$('#manual_event_submit_status').html('Thank you. Your event "' + new_event.name + '" has been added to the queue for approval. It will appear shortly.');
+			return false;
+		},
+		error: function (jqXHR, t, e) {
+			console.log('error');
+			$('#manual_event_submit_status').html('Something went wrong! Are you sure the event you submitted is public?');
+			return false;
+		}
+	});
+};
