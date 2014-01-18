bdf5a5d950d6d59fde45625b68ec456a7bad1c73
Joseph Long <josephoenix@gmail.com>
2014-01-18 15:54:53 -0500

    Have scrape_tweets print names of feeds as it goes

--- aspc/activityfeed/management/commands/scrape_twitter.py
+++ aspc/activityfeed/management/commands/scrape_twitter.py
@@ -15,16 +15,18 @@ class Command(BaseCommand):
                           consumer_secret=settings.TWITTER_CONSUMER_SECRET,
                           access_token_key=settings.TWITTER_ACCESS_TOKEN,
                           access_token_secret=settings.TWITTER_ACCESS_TOKEN_SECRET)
-        for a in settings.TWITTER_FEEDS:
+        for feedname in settings.TWITTER_FEEDS:
+            self.stdout.write("Getting latest tweets from @%s\n" % feedname)
+            self.stdout.flush()
             try:
-                latest_tweet_id = TwitterActivity.objects.filter(author=a)[0].tweet_id
+                latest_tweet_id = TwitterActivity.objects.filter(author=feedname)[0].tweet_id
             except:
                 latest_tweet_id = ''
-            new_tweets = api.GetUserTimeline(screen_name=a, since_id=latest_tweet_id,
+            new_tweets = api.GetUserTimeline(screen_name=feedname, since_id=latest_tweet_id,
                                              exclude_replies='true', include_rts='false')
             if new_tweets:
                 for tweet in new_tweets:
-                    url = 'http://twitter.com/%s/' % a
+                    url = 'http://twitter.com/%s/' % feedname
                     activity = TwitterActivity(url=url, message=tweet.text, author=tweet.user.screen_name,
                                                tweet_id=tweet.id, date=dateutil.parser.parse(tweet.created_at))
                     activity.save()
