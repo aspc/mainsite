e5e4e8ac51f778d136b2ce593ef19b15139b2750
Joseph Long <josephoenix@gmail.com>
2014-01-19 01:34:48 -0500

    QuerySet.select_related arguments have changed

--- aspc/housing/views.py
+++ aspc/housing/views.py
@@ -22,7 +22,7 @@ class Home(ArchiveIndexView):
     template_name = "housing/home.html"
     date_field = "create_ts"
     allow_empty = True
-    queryset = Review.objects.all().select_related(depth=3)
+    queryset = Review.objects.all().select_related('room')
     paginate_by = 15
 
 def format_data(rooms):
