4bac3efcd6485349e679efc88d3a2574efb52035
Matt Dahl <matt.dahl.2013@gmail.com>
2014-02-28 18:35:31 -0800

    Sets up basic markup for menu display; defines routes.

--- aspc/menu/urls.py
+++ aspc/menu/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import patterns, url
-from aspc.menu.views import home
+from aspc.menu.views import home, weekend, weekday
 
 urlpatterns = patterns('',
-	url(r'^$', home, name="menu")
+	url(r'', home, name='menu'),
+	url(r'mon', weekday, name='mon'),
+	url(r'tue', weekday, name='tue'),
+	url(r'wed', weekday, name='wed'),
+	url(r'thu', weekday, name='thu'),
+	url(r'fri', weekday, name='fri'),
+	url(r'sat', weekend, name='sat'),
+	url(r'sun', weekend, name='sun')
 )
--- aspc/menu/views.py
+++ aspc/menu/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # /menu
 def home (request):
-	if request.method == 'GET': # Render the menu index on GET
-		return render(request, 'menu/home.html', {})
+	if request.method == 'GET':
+		if datetime.today().weekday() < 5:
+			return weekday(request)
+		else:
+			return weekend(request)
+
+# /menu/{weekday}
+def weekday (request):
+	if request.method == 'GET':
+		return render(request, 'menu/weekday_menu.html')
+
+# /menu/{weekend}
+def weekend (request):
+	if request.method == 'GET':
+		return render(request, 'menu/weekend_menu.html')
