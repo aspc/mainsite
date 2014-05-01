a040eb42e6f40ce5965501041e4a4fc5080ac479
Matt Dahl <matt.dahl.2013@gmail.com>
2014-04-30 23:44:54 -0700

    Enables viewing of questions by id.

--- aspc/askasagehen/urls.py
+++ aspc/askasagehen/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 from aspc.askasagehen.views import home, question
 
 urlpatterns = patterns('',
-	url(r'question', question, name='submit_question'),
-	url(r'', home, name='askasagehen')
+	url(r'^question/(?P<question_id>\d+)?(/)?', question, name="question_detail"), # /question/123
+	url(r'question', question, {'question_id': None}, name='submit_question'), # /question
+	url(r'', home, name='askasagehen') # /
 )
--- aspc/askasagehen/views.py
+++ aspc/askasagehen/views.py
@@ -8,13 +8,15 @@ def home (request):
 	if request.method == 'GET':
 		return render(request, 'askasagehen/home.html', {'questions': Question.objects.all()})
 
-def question (request):
-	if request.method == 'POST': # If the form has been submitted...
-		form = QuestionForm(request.POST) # A form bound to the POST data
+def question (request, question_id):
+	if request.method == 'GET' and question_id: # Render an existing question
+		return render(request, 'askasagehen/question.html', {'question': Question.objects.get(id=question_id)})
+	elif request.method == 'GET': # Render the question submssion form
+		form = QuestionForm()
+		return render(request, 'askasagehen/submit_question.html', {'form': form})
+	elif request.method == 'POST': # Process the submitted form
+		form = QuestionForm(request.POST)
 		if form.is_valid():
 			form.instance.author = request.user
 			form.save()
 			return HttpResponseRedirect('/askasagehen/')
-	elif request.method == 'GET':
-		form = QuestionForm()
-		return render(request, 'askasagehen/submit_question.html', {'form': form})
