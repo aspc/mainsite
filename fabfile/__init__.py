2cbe0ae616cd2260288ceb21f973f0986bc1e2eb
Matt Dahl <matt.dahl.2013@gmail.com>
2014-09-28 23:41:09 -0700

    Changes fabfile to allow for staging of custom branches.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -17,20 +17,25 @@ Provides 'stage' as an alias for 'on_staging apply_changes'
 and 'deploy' for 'on_main apply_changes'
 """
 
-def on_staging():
+def on_staging(branch):
     env.site = "staging"
-    env.branch = "staging"
+    env.branch = branch
 
 def on_main():
     env.site = "main"
     env.branch = "production"
 
-def stage():
-    on_staging()
+# Running `fab stage` like normal will merge master into staging and deploy that branch on staging.aspc.pomona.edu
+# Passing an argument - a specific branch name - will deploy *that* branch instead on staging.aspc.pomona.edu
+def stage(branch="staging"):
+    on_staging(branch=branch)
+    git_pull()
     apply_changes()
 
 def deploy():
     on_main()
+    git_merge_push()
+    git_pull()
     apply_changes()
 
 def migrate():
@@ -58,7 +63,7 @@ def install_requirements():
 def reload():
     run("/srv/www/{0}/bin/gunicorn.sh reload".format(env.site))
 
-def git_push_pull():
+def git_merge_push():
     local("git checkout master")
     local("git push {0} master".format(env.origin))
     local("git checkout {0}".format(env.branch))
@@ -66,16 +71,19 @@ def git_push_pull():
     local("git push {0} {1}".format(env.origin, env.branch))
     local("git checkout master")
     local("git status")
+
+def git_pull():
     with settings(
         cd("/srv/www/{0}/mainsite".format(env.site)),
     ):
         sudo("git clean -f", user=env.site)
         sudo("git checkout -f .", user=env.site)
-        sudo("git pull origin", user=env.site)
+        sudo("git fetch", user=env.site)
+        sudo("git checkout {0}".format(env.branch), user=env.site)
+        sudo("git pull origin {0}".format(env.branch), user=env.site)
         sudo("git status", user=env.site)
 
 def apply_changes():
-    git_push_pull()
     install_requirements()
     update_static()
     migrate()
