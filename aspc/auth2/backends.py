25283295242fb6439e16a23951b4cc4fb904d6cc
Matt Dahl <matt.dahl.2013@gmail.com>
2014-10-02 22:24:45 -0700

    Cleans up CAS implementation in auth2 module.
    Fix settings var typo.
    
    Debug XML response.

--- aspc/auth2/backends.py
+++ aspc/auth2/backends.py
@@ -1,192 +1,77 @@
-"""CAS authentication backend"""
-
 import logging
 from urllib import urlencode, urlopen
 from urlparse import urljoin
 from xml.dom import minidom
-
+from xml.etree import ElementTree
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
-from aspc.auth2.exceptions import CasTicketException
-from aspc.auth2.models import User, Tgt, PgtIOU
-from aspc.auth2.utils import cas_response_callbacks
+from django.contrib.auth.models import User
 
 __all__ = ['CASBackend']
 
 logger = logging.getLogger(__name__)
 
-def _verify_cas1(ticket, service):
-    """Verifies CAS 1.0 authentication ticket.
-
-    Returns username on success and None on failure.
-    """
-
-    params = {'ticket': ticket, 'service': service}
-    url = (urljoin(settings.CAS_SERVER_URL, 'validate') + '?' +
-           urlencode(params))
-    page = urlopen(url)
-    try:
-        verified = page.readline().strip()
-        if verified == 'yes':
-            return page.readline().strip()
-        else:
-            return None
-    finally:
-        page.close()
-
-
-def _verify_cas2(ticket, service):
-    """Verifies CAS 2.0+ XML-based authentication ticket.
-
-    Returns username on success and None on failure.
-    """
-
-    try:
-        from xml.etree import ElementTree
-    except ImportError:
-        from elementtree import ElementTree
-
-    params = {'ticket': ticket, 'service': service}
-    if settings.CAS_PROXY_CALLBACK:
-        params['pgtUrl'] = settings.CAS_PROXY_CALLBACK
-
-    url = urljoin(settings.CAS_SERVER_URL, 'serviceValidate') + '?' + urlencode(params)
-
-    page = urlopen(url)
-
-    username = None
-
-    try:
-        response = page.read()
-        tree = ElementTree.fromstring(response)
-        document = minidom.parseString(response)
-
-        #Useful for debugging
-        #print document.toprettyxml()
-
-        if tree[0].tag.endswith('authenticationSuccess'):
-            if settings.CAS_RESPONSE_CALLBACKS:
-                cas_response_callbacks(tree)
-
-            username = tree[0][0].text
-
-            pgt_el = document.getElementsByTagName('cas:proxyGrantingTicket')
-            if pgt_el:
-                pgt = pgt_el[0].firstChild.nodeValue
-                try:
-                    pgtIou = _get_pgtiou(pgt)
-                    tgt = Tgt.objects.get(username=username)
-                    tgt.tgt = pgtIou.tgt
-                    tgt.save()
-                    pgtIou.delete()
-                except Tgt.DoesNotExist:
-                    Tgt.objects.create(username=username, tgt=pgtIou.tgt)
-                    pgtIou.delete()
-                except Exception:
-                    logger.error('Failed to do proxy authentication.')
-
-        else:
-            failure = document.getElementsByTagName('cas:authenticationFailure')
-            if failure:
-                logger.warn('Authentication failed from CAS server: %s',
-                            failure[0].firstChild.nodeValue)
-
-    except Exception as e:
-        logger.error('Failed to verify CAS authentication', e)
-
-    finally:
-        page.close()
-
-    return username
-
-
-def verify_proxy_ticket(ticket, service):
-    """Verifies CAS 2.0+ XML-based proxy ticket.
-
-    Returns username on success and None on failure.
-    """
-
-    try:
-        from xml.etree import ElementTree
-    except ImportError:
-        from elementtree import ElementTree
-
-    params = {'ticket': ticket, 'service': service}
-
-    url = (urljoin(settings.CAS_SERVER_URL, 'proxyValidate') + '?' +
-           urlencode(params))
-
-    page = urlopen(url)
-
-    try:
-        response = page.read()
-        tree = ElementTree.fromstring(response)
-        if tree[0].tag.endswith('authenticationSuccess'):
-            username = tree[0][0].text
-            proxies = []
-            if len(tree[0]) > 1:
-                for element in tree[0][1]:
-                    proxies.append(element.text)
-            return {"username": username, "proxies": proxies}
-        else:
-            return None
-    finally:
-        page.close()
-
-_PROTOCOLS = {'1': _verify_cas1, '2': _verify_cas2}
-
-if settings.CAS_VERSION not in _PROTOCOLS:
-    raise ValueError('Unsupported CAS_VERSION %r' % settings.CAS_VERSION)
-
-_verify = _PROTOCOLS[settings.CAS_VERSION]
-
-
-def _get_pgtiou(pgt):
-    """ Returns a PgtIOU object given a pgt.
-
-        The PgtIOU (tgt) is set by the CAS server in a different request
-        that has completed before this call, however, it may not be found in
-        the database by this calling thread, hence the attempt to get the
-        ticket is retried for up to 5 seconds. This should be handled some
-        better way.
-    """
-    pgtIou = None
-    retries_left = 5
-    while not pgtIou and retries_left:
-        try:
-            return PgtIOU.objects.get(tgt=pgt)
-        except PgtIOU.DoesNotExist:
-            time.sleep(1)
-            retries_left -= 1
-    raise CasTicketException("Could not find pgtIou for pgt %s" % pgt)
-
-
+# CAS authentication backend, authenticating against the 5C CAS server
 class CASBackend(object):
-    """CAS authentication backend"""
+	supports_object_permissions = False
+	supports_inactive_user = False
 
-    supports_object_permissions = False
-    supports_inactive_user = False
+	# Vertifies CAS ticket and gets or creates User object
+	def authenticate(self, ticket, service):
+		return _verify_cas(ticket, service)
+		username = _verify_cas(ticket, service)
+		if not username:
+			return None
+		try:
+			user = User.objects.get(username__iexact=username)
+		except User.DoesNotExist:
+			user = User(
+				username=username,
+				first_name=None,
+				last_name=None,
+				email=None,
+			)
+			user.set_unusable_password()
+			user.save()
+		return user
 
-    def authenticate(self, ticket, service):
-        """Verifies CAS ticket and gets or creates User object
-            NB: Use of PT to identify proxy
-        """
+	def get_user(self, user_id):
+		try:
+			return User.objects.get(pk=user_id)
+		except User.DoesNotExist:
+			return None
 
-        username = _verify(ticket, service)
-        if not username:
-            return None
-        try:
-            user = User.objects.get(username__iexact=username)
-        except User.DoesNotExist:
-            # user will have an "unusable" password
-            user = User.objects.create_user(username, '')
-            user.save()
-        return user
+# Verifies CAS 2.0+ XML-based authentication ticket
+# Returns username on success and None on failure
+def _verify_cas(ticket, service):
+	params = {
+		'ticket': ticket,
+		'service': service
+	}
 
-    def get_user(self, user_id):
-        """Retrieve the user's entry in the User model if it exists"""
+	url = urljoin(settings.CAS_SETTINGS['SERVER_URL'], 'serviceValidate') + '?' + urlencode(params)
+	page = urlopen(url)
+	username = None
 
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
+	try:
+		response = page.read()
+		tree = ElementTree.fromstring(response)
+		document = minidom.parseString(response)
+
+		#Useful for debugging
+		#print document.toprettyxml()
+
+		if tree[0].tag.endswith('authenticationSuccess'):
+			username = tree[0][0].text
+
+		else:
+			failure = document.getElementsByTagName('cas:authenticationFailure')
+			if failure:
+				logger.warn('Authentication failed from CAS server: %s', failure[0].firstChild.nodeValue)
+
+	except Exception as e:
+		logger.error('Failed to verify CAS authentication', e)
+
+	finally:
+		page.close()
+
+	return str(response)
--- aspc/auth2/middleware.py
+++ aspc/auth2/middleware.py
@@ -1,5 +1,3 @@
-"""CAS authentication middleware"""
-
 from urllib import urlencode
 
 from django.conf import settings
@@ -8,68 +6,50 @@ from django.contrib.auth import logout as do_logout
 from django.contrib.auth.views import login, logout
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponseForbidden
-from django.core.exceptions import ImproperlyConfigured
 
 from aspc.auth2.exceptions import CasTicketException
 from aspc.auth2.views import login as cas_login, logout as cas_logout
 
 __all__ = ['CASMiddleware']
 
+# Middleware that allows CAS authentication on admin pages
 class CASMiddleware(object):
-    """Middleware that allows CAS authentication on admin pages"""
+	# Checks that the authentication middleware is installed
+	def process_request(self, request):
+		error = "The Django CAS middleware requires authentication middleware to be installed. Edit your MIDDLEWARE_CLASSES setting to insert 'django.contrib.auth.middleware.AuthenticationMiddleware'."
+		assert hasattr(request, 'user'), error
 
-    def process_request(self, request):
-        """Checks that the authentication middleware is installed"""
+	# Forwards unauthenticated requests to the admin page to the CAS login URL,
+	# as well as calls to django.contrib.auth.views.login and logout.
+	def process_view(self, request, view_func, view_args, view_kwargs):
+		if view_func == login:
+			return cas_login(request, *view_args, **view_kwargs)
+		elif view_func == logout:
+			return cas_logout(request, *view_args, **view_kwargs)
 
-        error = ("The Django CAS middleware requires authentication "
-                 "middleware to be installed. Edit your MIDDLEWARE_CLASSES "
-                 "setting to insert 'django.contrib.auth.middleware."
-                 "AuthenticationMiddleware'.")
-        assert hasattr(request, 'user'), error
+		if settings.CAS_SETTINGS['ADMIN_PREFIX']:
+			if not request.path.startswith(settings.CAS_SETTINGS['ADMIN_PREFIX']):
+				return None
+		elif not view_func.__module__.startswith('django.contrib.admin.'):
+			return None
 
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        """Forwards unauthenticated requests to the admin page to the CAS
-        login URL, as well as calls to django.contrib.auth.views.login and
-        logout.
-        """
+		if request.user.is_authenticated():
+			if request.user.is_staff:
+				return None
+			else:
+				error = ('<h1>Forbidden</h1><p>You do not have staff privileges.</p>')
+				return HttpResponseForbidden(error)
 
-        if view_func == login:
-            return cas_login(request, *view_args, **view_kwargs)
-        elif view_func == logout:
-            return cas_logout(request, *view_args, **view_kwargs)
+		params = urlencode({REDIRECT_FIELD_NAME: request.get_full_path()})
+		return HttpResponseRedirect(reverse(cas_login) + '?' + params)
 
-        if settings.CAS_ADMIN_PREFIX:
-            if not request.path.startswith(settings.CAS_ADMIN_PREFIX):
-                return None
-        elif not view_func.__module__.startswith('django.contrib.admin.'):
-            return None
+	def process_exception(self, request, exception):
+		# When we get a CasTicketException, that is probably caused by the ticket timing out.
+		# So logout/login and get the same page again.
 
-        if request.user.is_authenticated():
-            if request.user.is_staff:
-                return None
-            else:
-                error = ('<h1>Forbidden</h1><p>You do not have staff '
-                         'privileges.</p>')
-                return HttpResponseForbidden(error)
-        params = urlencode({REDIRECT_FIELD_NAME: request.get_full_path()})
-        return HttpResponseRedirect(reverse(cas_login) + '?' + params)
-
-    def process_exception(self, request, exception):
-        """When we get a CasTicketException, that is probably caused by the ticket timing out.
-        So logout/login and get the same page again."""
-        if isinstance(exception, CasTicketException):
-            do_logout(request)
-            # This assumes that request.path requires authentication.
-            return HttpResponseRedirect(request.path)
-        else:
-            return None
-
-class ProxyMiddleware(object):
-
-    # Middleware used to "fake" the django app that it lives at the Proxy Domain
-    def process_request(self, request):
-        proxy = getattr(settings, 'PROXY_DOMAIN', None)
-        if not proxy:
-            raise ImproperlyConfigured('To use Proxy Middleware you must set a PROXY_DOMAIN setting.')
-        else:
-            request.META['HTTP_HOST'] = proxy
+		if isinstance(exception, CasTicketException):
+			do_logout(request)
+			# This assumes that request.path requires authentication.
+			return HttpResponseRedirect(request.path)
+		else:
+			return None
