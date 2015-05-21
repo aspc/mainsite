b354025da654751ab579f8c5a15c3fd22ea4befa
Matt Dahl <matt.dahl.2013@gmail.com>
2015-05-21 16:33:37 -0400

    Updates package references to lorem_ipsum Django module.

--- aspc/events/management/commands/fakeevents.py
+++ aspc/events/management/commands/fakeevents.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.contrib.webdesign import lorem_ipsum
+from django.utils import lorem_ipsum
 from aspc.events.models import Event
 import datetime
 import random
@@ -7,7 +7,7 @@ import random
 class Command(BaseCommand):
     args = '<events per day>'
     help = 'generates some fake events'
-    
+
     _locations = (
         'Doms Lounge',
         'SCC 207',
@@ -15,7 +15,7 @@ class Command(BaseCommand):
         'Big Bridges',
         'Pomona College Museum of Art',
     )
-    
+
     _hosts = (
         'Pomona Events Committee',
         'Cheese Club',
@@ -23,7 +23,7 @@ class Command(BaseCommand):
         'Career Development Office',
         'Bottom Line Theatre',
     )
-    
+
     _adjectives = (
         'Awesome',
         'New',
@@ -32,7 +32,7 @@ class Command(BaseCommand):
         'Inaugural',
         'Substance-free',
     )
-    
+
     _nouns = (
         'Table Manners',
         'Kitten',
@@ -40,7 +40,7 @@ class Command(BaseCommand):
         'Live Music',
         'Digital',
     )
-    
+
     _types = (
         'Exhibition',
         'Performance',
@@ -56,7 +56,7 @@ class Command(BaseCommand):
             num_events = int(args[0])
         else:
             num_events = 10
-        
+
         self.stdout.write('Generating %i fake events for the next 5 days\n' % (num_events,))
         today = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
         for day in range(6):
@@ -67,7 +67,7 @@ class Command(BaseCommand):
                     noun=random.choice(self._nouns),
                     type=random.choice(self._types)
                 )
-                
+
                 new_event.start = today + datetime.timedelta(days=day, hours=8 + evtnum)
                 new_event.end = new_event.start + datetime.timedelta(minutes=30)
                 new_event.location = random.choice(self._locations)
@@ -75,7 +75,6 @@ class Command(BaseCommand):
                 new_event.host = random.choice(self._hosts)
                 new_event.url = "http://aspc.pomona.edu/"
                 new_event.status = 'approved'
-                
+
                 new_event.save()
                 self.stdout.write("Saved event '%s'" % (new_event.name,))
-        
