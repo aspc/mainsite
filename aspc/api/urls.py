dfcded45c4d64d3adb8cb78255bafb04ea0a6184
theworldbright <kent@kentshikama.com>
2015-11-11 22:05:43 -0800

    Minor URL fix for API

--- aspc/api/urls.py
+++ aspc/api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'menu/dining_hall/(?P<dining_hall>[^/]+)/day/(?P<day>[^/]+)/$', MenuDiningHallDayDetail.as_view()),
     url(r'menu/day/(?P<day>[^/]+)/$', MenuDayDetail.as_view()),
     url(r'menu/dining_hall/(?P<dining_hall>[^/]+)/$', MenuDiningHallDetail.as_view()),
-    url(r'menu$', MenuList.as_view()),
+    url(r'menu/$', MenuList.as_view()),
     url(r'^$', views.api_home, name="api_home"),
 ]
