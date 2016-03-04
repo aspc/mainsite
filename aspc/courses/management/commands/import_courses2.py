ab7682e09e04b0d67e8422da0d0e31e6a4c13e44
Matt Dahl <matt.dahl.2013@gmail.com>
2016-03-04 14:04:39 -0800

    Refactors the import of Meeting data.

--- aspc/courses/management/commands/import_courses2.py
+++ aspc/courses/management/commands/import_courses2.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.template.defaultfilters import slugify
 import simplejson, urllib, re
+from datetime import time
 import logging
 
 from aspc.courses.models import Course, Meeting, CAMPUSES_LOOKUP, Term, Section, Department, Instructor, RequirementArea, RefreshHistory
@@ -11,6 +12,8 @@ TERMS_URL = settings.COURSE_API_URL + 'terms'
 
 FEE_REGEX = re.compile(r'[Ff]ee:\s+\$([\d\.]+)')
 BR_TAGS_REGEX = re.compile(r'<br\s?/?>')
+ROOM_REGEX = re.compile(r'[A-Z]+\s([^(]+)\s+')
+TIME_REGEX = re.compile(r'(\d+:\d+)(AM|PM)?-(\d+:\d+)(AM|PM).')
 
 logger = logging.getLogger(__name__)
 
@@ -103,6 +106,14 @@ class Command(BaseCommand):
 					)
 					course_object.number = int(''.join([s for s in course_code if s.isdigit()]))
 					course_object.departments.add(Department.objects.get(code=department_code))
+
+					primary_department_code = course_code[:4].strip() # Department codes are 3 or 4 characters long
+					try:
+						course_object.primary_department = Department.objects.get(code=primary_department_code)
+					except Department.DoesNotExist:
+						log_error('primary department {0} does not exist for course {1}'.format(primary_department_code, course_code))
+						course_object.primary_department = course_object.departments.all()[0] # Just set the primary department to the course's first department instead
+
 					course_object.save()
 
 					if was_created:
@@ -123,6 +134,31 @@ class Command(BaseCommand):
 					section_object.requisites = section_data['Requisites'] == 'Y'
 					section_object.fee = FEE_REGEX.findall(unicode(section_data['Description']))
 					section_object.grading_style = sanitize(section_data['GradingStyle'])
+
+					if section_data['Instructors']:
+						for instructor in section_data['Instructors']:
+							instructor_object, _ = Instructor.objects.get_or_create(name=instructor['Name'])
+							section_object.instructors.add(instructor_object)
+
+					if section_data['Schedules']:
+						for meeting in section_data['Schedules']:
+							if meeting['Weekdays'] != '':
+								parsed_meeting_data = self._parse_meeting_data(meeting)
+								if parsed_meeting_data:
+									meeting_object = Meeting(
+										section=section_object,
+										monday=parsed_meeting_data['monday'],
+										tuesday=parsed_meeting_data['tuesday'],
+										wednesday=parsed_meeting_data['wednesday'],
+										thursday=parsed_meeting_data['thursday'],
+										friday=parsed_meeting_data['friday'],
+										begin=parsed_meeting_data['begin'],
+										end=parsed_meeting_data['end'],
+										campus=parsed_meeting_data['campus'],
+										location=parsed_meeting_data['location']
+									)
+									meeting_object.save()
+
 					section_object.save()
 
 					if was_created:
@@ -148,3 +184,71 @@ class Command(BaseCommand):
 			for section_object in section_objects:
 				# Append the relevant AreaRequirement object to that Section
 				section_object.course.requirement_areas.add(area_object)
+
+	def _parse_meeting_data(self, meeting):
+		# Parse weekdays
+		weekdays = meeting['Weekdays']
+		monday = True if weekdays.find('M') != -1 else False
+		tuesday = True if weekdays.find('T') != -1 else False
+		wednesday = True if weekdays.find('W') != -1 else False
+		thursday = True if weekdays.find('R') != -1 else False
+		friday = True if weekdays.find('F') != -1 else False
+
+		# Parse times
+		try:
+			start, start_pm, end, end_pm = TIME_REGEX.findall(meeting['MeetTime'])[0]
+		except IndexError:
+			return None
+
+		# Set end_pm
+		if end_pm == 'PM':
+			end_pm = True
+		else:
+			end_pm = False
+
+		# Set start_pm
+		if start_pm in ('AM', 'PM'):
+			start_pm = True if start_pm == 'PM' else False
+		else:
+			start_pm = end_pm
+
+		start_h, start_m = [int(a) for a in start.split(':')]
+		end_h, end_m = [int(a) for a in end.split(':')]
+
+		# Correct times to 24hr form
+		if end_pm and end_h != 12:
+			end_h += 12
+		if start_pm and start_h != 12:
+			start_h += 12
+		begin = time(start_h, start_m)
+		end = time(end_h, end_m)
+
+		# Get campus
+		try:
+			campus_code = meeting['Campus'].split(' ')[0]
+			campus = CAMPUSES_LOOKUP[campus_code]
+		except Exception:
+			campus = CAMPUSES_LOOKUP['?']
+
+		# Get location
+		if meeting['Room'] and meeting['Building']:
+			room_number = ROOM_REGEX.findall(meeting['MeetTime']) # Returns a list
+			room_number = room_number[0] if room_number else ''
+			location = "{0}, {1}".format(meeting['Building'], room_number)
+			# special case for Keck building / CU campus
+			if meeting['Building'] == u'Keck Science Center':
+				campus = CAMPUSES_LOOKUP['KS']
+		else:
+			location = ''
+
+		return {
+			'monday': monday,
+			'tuesday': tuesday,
+			'wednesday': wednesday,
+			'thursday': thursday,
+			'friday': friday,
+			'begin': begin,
+			'end': end,
+			'campus': campus,
+			'location': location
+		}
