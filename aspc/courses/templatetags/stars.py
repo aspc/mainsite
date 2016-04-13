47cf52319da69956b3d8abffcd1cde67f72a67df
Aloke Desai <aloke.desai@gmail.com>
2016-04-13 11:44:50 -0700

    change course reviews stars to use different, more modern, icon

--- aspc/courses/templatetags/stars.py
+++ aspc/courses/templatetags/stars.py
@@ -10,7 +10,8 @@ def star(rating):
     num_star_on = int(math.floor(rating))
     num_star_half = int(round(rating)) - num_star_on
     num_star_off = 5 - num_star_on - num_star_half
-    html = num_star_on * ('<img src="'+STATIC_URL+'images/star-on.gif">') + \
-        num_star_half * ('<img src="'+STATIC_URL+'images/star-halfon.gif">') + \
-        num_star_off * ('<img src="'+STATIC_URL+'images/star-off.gif">')
+
+    html = num_star_on * ('<img height=15 src="'+STATIC_URL+'images/star-filled.png">') + \
+        num_star_half * ('<img height=15 src="'+STATIC_URL+'images/star-half.png">') + \
+        num_star_off * ('<img height=15 src="'+STATIC_URL+'images/star-empty.png">')
     return mark_safe(html)
