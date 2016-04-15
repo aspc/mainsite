4e96127ef5e003c2beaafd46d26a72a88e510281
Matt Dahl <matt.dahl.2013@gmail.com>
2016-04-14 18:17:45 -0700

    Fixes JS toggler.

--- aspc/static/js/reviews_search.js
+++ aspc/static/js/reviews_search.js
@@ -4,8 +4,22 @@ ASPC.ReviewsSearch = ASPC.ReviewsSearch || {};
 $(document).ready(function () {
 	ASPC.ReviewsSearch.dropdown = $('#object_type_dropdown');
 
+	if (ASPC.ReviewsSearch.dropdown[0].value == 'course') {
+		$('#review_search_form-professorname').toggleClass('hidden');
+	}
+	else if (ASPC.ReviewsSearch.dropdown[0].value == 'professor') {
+		$('#review_search_form-coursename').toggleClass('hidden');
+	}
+
 	ASPC.ReviewsSearch.dropdown.on('change', function () {
 		$('#review_search_form-coursename').toggleClass('hidden');
 		$('#review_search_form-professorname').toggleClass('hidden');
+
+		if (ASPC.ReviewsSearch.dropdown[0].value == 'course') {
+			$('#review_search_form-professorname input')[0].value = '';
+		}
+		else if (ASPC.ReviewsSearch.dropdown[0].value == 'professor') {
+			$('#review_search_form-coursename input')[0].value = '';
+		}
 	});
 });
