aca9a587cf88d9bf0196265976896954411ddac9
Matt Dahl <matt.dahl.2013@gmail.com>
2016-04-17 18:51:49 -0700

    Flips enthusiasm and approachability description text.

--- aspc/courses/forms.py
+++ aspc/courses/forms.py
@@ -335,8 +335,8 @@ class ReviewForm(forms.Form):
     difficulty_rating = forms.ChoiceField(choices=CHOICES, label='How difficult was this class?', help_text='1: I could do the homework in my sleep <br />5: I still don\'t understand what I did in this course')
     competency_rating = forms.ChoiceField(choices=CHOICES, label='How competent was the professor?', help_text='1: The prof had no clue <br />5: The prof knew the material backwards and forwards')
     lecturing_rating = forms.ChoiceField(choices=CHOICES, label='How was the professor\'s lecturing style?', help_text='1: Lectures were poorly planned and delivered <br />5: I remember every word the prof said')
-    enthusiasm_rating = forms.ChoiceField(choices=CHOICES, label='How enthusiastic was the professor?', help_text='1: I would rather speak to Darth Vader <br />5: I consider this prof a personal friend')
-    approachable_rating = forms.ChoiceField(choices=CHOICES, label='How approachable was the professor?', help_text='1: The prof had no pulse <br />5: The prof\'s excitement was infectious')
+    enthusiasm_rating = forms.ChoiceField(choices=CHOICES, label='How enthusiastic was the professor?', help_text='1: The prof had no pulse <br />5: The prof\'s excitement was infectious')
+    approachable_rating = forms.ChoiceField(choices=CHOICES, label='How approachable was the professor?', help_text='1: I would rather speak to Darth Vader <br />5: I consider this prof a personal friend')
     work_per_week = forms.IntegerField(max_value=25, label='How many hours of work did you have each week?')
     comments = forms.CharField(widget=forms.Textarea(attrs={'cols': '60', 'rows': '15'}), label='General comments:')
 
