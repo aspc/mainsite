bdb455fd2cf7f2a7331a0e241559de323e3473e6
theworldbright <kent@kentshikama.com>
2016-04-24 01:28:42 -0700

    Show loading gif on pagination button press

--- aspc/static/js/search_result.js
+++ aspc/static/js/search_result.js
@@ -1,21 +1,26 @@
-function add_listener(){
-	$('.pagi-btn').click(function(){
-		var path = $(this).attr('path');
-		$(this).hide();
-		$.ajax({
-			url : path,
-			success : function(data){
-				$('.course_list').replaceWith(data);
-				window.history.pushState("new page", "Course Planner", path);
-				add_listener();
-			},
-			error: function(error){
-				$(this).show()
-			}
-		})
-	})
+function add_listener() {
+    $('.pagi-btn').click(function () {
+        var path = $(this).attr('path');
+        if (pagination_request_not_already_sent.call(this)) {
+            $(this).html('<img src="' + ASPC.Settings.static_url + '/images/loading.gif" width="13" />');
+            $.ajax({
+                       url: path,
+                       success: function (data) {
+                           $('.course_list').replaceWith(data);
+                           window.history.pushState("new page", "Course Planner", path);
+                           add_listener();
+                       },
+                       error: function (error) {
+                           $(this).show()
+                       }
+                   });
+        }
+        function pagination_request_not_already_sent() {
+            return $(this).html().indexOf("img") == -1;
+        }
+    })
 }
 
 $('document').ready(function () {
-	add_listener();
+    add_listener();
 });
