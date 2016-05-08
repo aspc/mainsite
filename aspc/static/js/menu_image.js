329a65c617fca3fac94bc870ab313aff624df087
ZiqiXiong <zx012013@mymail.pomona.edu>
2016-05-07 22:31:10 -0700

    remove ajax from menu_image.js

--- aspc/static/js/menu_image.js
+++ aspc/static/js/menu_image.js
@@ -1,39 +1,26 @@
 $('#menu_table tr td ul li').hover(function(){
         var item = $(this);
-        var name = item.text();
-        $(this).append("<a href='javascript:void(0)' class='image_link'></a>");
-        $('.image_link').click(function(event){
-            $.ajax({
-                url : 'image',
-                data: {name: name},
-                success : function(data){
-                    console.log(data);
-                    item.qtip({content: '<img width="100px" height="100px" src="'+data+'">',
-                                position: {
-                                  at: 'bottom center'
-                                },
-                                show: {
-                                  event: "mouseover",
-                                  solo: true,
-                                  ready: true
-                                },
-                                hide: {
-                                  event: 'mouseout'
-                                },
-                                style: {
-                                  classes: 'qtip-bootstrap',
-                                  tip: {
-                                      width: 16,
-                                      height: 8
-                                  }
-                          }
-                    },event)
+        var image_url = item.attr('image_url');
+            item.qtip({content: '<img width="100px" height="100px" src="'+image_url+'">',
+                position: {
+                    at: 'bottom center'
                 },
-                error: function(error){
-                    console.log('error');
+                show: {
+                    event: "mouseover",
+                    solo: true,
+                    ready: true
+                },
+                hide: {
+                    event: 'mouseout'
+                },
+                style: {
+                    classes: 'qtip-bootstrap',
+                    tip: {
+                        width: 16,
+                        height: 8
+                    }
                 }
-            })
-        })
+            },event)
     }
     ,function(){
         $('.image_link').remove();
