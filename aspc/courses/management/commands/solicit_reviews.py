de8d4297497cdac05f1c20e7367d7d3393aafd5d
Ziqi Xiong <zx012013@mymail.pomona.edu>
2016-10-17 05:11:49 +0000

    Allow users to subscribe

--- aspc/courses/management/commands/solicit_reviews.py
+++ aspc/courses/management/commands/solicit_reviews.py
@@ -9,7 +9,7 @@ from aspc.settings import EMAIL_HOST_USER
 # Assume we send the emails at the end of the semester, we
 # should only consider schedules that are at least 3 months old
 MIN_DAYS = 90
-MAX_DAYS = 700
+MAX_DAYS = 300
 EMAIL_TITLE = "Have you taken these classes?"
 
 class Command(BaseCommand):
@@ -21,15 +21,19 @@ class Command(BaseCommand):
         htmly     = get_template('email/solicit_reviews.html')
         schedules = Schedule.objects.filter(create_ts__lte=datetime.now()-timedelta(days=MIN_DAYS),
                                             create_ts__gte=datetime.now()-timedelta(days=MAX_DAYS))
+        emails_sent = 0
         for schedule in schedules:
             try:
                 context = Context({'user': schedule.user, 'courses': schedule.sections.all()})
                 text_content = plaintext.render(context)
                 html_content = htmly.render(context)
-                msg = EmailMultiAlternatives(EMAIL_TITLE, text_content, EMAIL_HOST_USER, [schedule.user.email])
-                msg.attach_alternative(html_content, "text/html")
-                msg.send()
+                user_data = schedule.user.user.all()
+                if user_data and user_data[0].subscribed_email:
+                    msg = EmailMultiAlternatives(EMAIL_TITLE, text_content, EMAIL_HOST_USER, [schedule.user.email])
+                    msg.attach_alternative(html_content, "text/html")
+                    msg.send()
+                    emails_sent += 1
             except Exception as e:
                 self.stdout.write('Error: %s\n' % e)
-        self.stdout.write('Successfully send %s emails\n' % len(schedules))
+        self.stdout.write('Successfully send %s emails\n' % emails_sent)
 
