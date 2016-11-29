b2182a63248a0e541b13dc31d1a2022559a351c7
Ziqi Xiong <zx012013@mymail.pomona.edu>
2016-11-29 05:32:08 +0000

    Create some skeleton code

--- aspc/mentalhealth/views.py
+++ aspc/mentalhealth/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from aspc.mentalhealth.models import Therapist
 
 # Create your views here.
+
+def home(request):
+    therapists = Therapist.objects.all()
+    return render(request, 'mentalhealth_home.html', {'therapists': therapists})
+
+
