f7b16e8be93e5733b0f8a4e083744db8d0a40dfa
Ziqi Xiong <zx012013@mymail.pomona.edu>
2016-12-15 04:58:05 +0000

    Switch to Google API to load menu images

--- aspc/menu/management/commands/load_images.py
+++ aspc/menu/management/commands/load_images.py
@@ -1,8 +1,8 @@
 from django.core.management.base import BaseCommand
 from aspc.menu.models import Menu, Item
 import grequests, json
-# from aspc.settings import GOOGLE_SEARCH_KEY, GOOGLE_SEARCH_CX
-from aspc.settings import BING_SEARCH_KEY
+from aspc.settings import GOOGLE_SEARCH_KEY, GOOGLE_SEARCH_CX
+# from aspc.settings import BING_SEARCH_KEY
 from urllib import urlencode
 
 
@@ -20,16 +20,16 @@ class Command(BaseCommand):
             return [l[i:i+n] for i in xrange(0, len(l), n)]
 
         def process_items(items):
-            # GOOGLE_SEARCH_URL = 'https://www.googleapis.com/customsearch/v1?'
-            BING_SEARCH_URL = 'https://api.datamarket.azure.com/Bing/Search/v1/Image?$format=json&'
+            GOOGLE_SEARCH_URL = 'https://www.googleapis.com/customsearch/v1?'
+            # BING_SEARCH_URL = 'https://api.datamarket.azure.com/Bing/Search/v1/Image?$format=json&'
             unsent_requests, updated_items = [], []
             for item in items:
                 if not item.image_url:
-                    # params = {'q': item.name, 'key': GOOGLE_SEARCH_KEY, 'cx':GOOGLE_SEARCH_CX, 'num':1, 'searchType': "image"}
-                    params = {'Query':"'"+item.name.encode('utf-8')+"'"}
-                    # unsent_requests.append(grequests.get(url=GOOGLE_SEARCH_URL,  params=params))
-                    unsent_requests.append(grequests.get(url=BING_SEARCH_URL+urlencode(params),
-                                                         auth=(BING_SEARCH_KEY,BING_SEARCH_KEY)))
+                    params = {'q': item.name, 'key': GOOGLE_SEARCH_KEY, 'cx':GOOGLE_SEARCH_CX, 'num':1, 'searchType': "image"}
+                    # params = {'Query':"'"+item.name.encode('utf-8')+"'"}
+                    unsent_requests.append(grequests.get(url=GOOGLE_SEARCH_URL,  params=params))
+                    # unsent_requests.append(grequests.get(url=BING_SEARCH_URL+urlencode(params),
+                    #                                      auth=(BING_SEARCH_KEY,BING_SEARCH_KEY)))
                     updated_items.append(item)
             responses = grequests.map(unsent_requests)
             item_response_pairs = list(zip(updated_items, responses))
@@ -37,8 +37,8 @@ class Command(BaseCommand):
                 data = json.loads(resp.text)
                 print(data)
                 try:
-                    # link = data['items'][0]['link']
-                    link = data['d']['results'][0]['Thumbnail']['MediaUrl']
+                    link = data['items'][0]['thumbnailLink']
+                    # link = data['d']['results'][0]['Thumbnail']['MediaUrl']
                 except Exception as e:
                     link = ''
                 item.image_url = link
