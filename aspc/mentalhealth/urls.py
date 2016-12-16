a77cafabc4ffb0acdfdb2a5402ea973fac7289b7
Kent Shikama <kent@kentshikama.com>
2016-12-16 13:52:03 -0800

    Fix review forms for mh
    - Refactor to use model form
    - Add appropriate

--- aspc/mentalhealth/urls.py
+++ aspc/mentalhealth/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
-from aspc.mentalhealth.views import home
+from aspc.mentalhealth.views import home, ReviewView
 
 urlpatterns = [
     url(r'^$', home, name="mentalhealth_home"),
-
+    url(r'^review/?$', ReviewView.as_view(), name="review_create"),
+    url(r'^review/(?P<review_id>\d+)/?$', ReviewView.as_view(), name="review_edit"),
+    #url(r'^review$', login_required(ReviewView.as_view()), name="review"),
 ]
