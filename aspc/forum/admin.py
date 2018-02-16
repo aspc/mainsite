c2920572062e2abe846bad062a306ffe76b5271a
Chris <yg012014@mymail.pomona.edu>
2018-02-15 19:39:15 -0800

    Add student contact

--- aspc/forum/admin.py
+++ aspc/forum/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from aspc.forum.models import (Tag, Answer, Question, Answer)
 
-admin.site.register(Tag)
-admin.site.register(Answer)
-admin.site.register(Question)
-admin.site.register(Answer)
+#admin.site.register(Tag)
+#admin.site.register(Answer)
+#admin.site.register(Question)
+#admin.site.register(Answer)
