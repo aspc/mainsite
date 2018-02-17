205422f3b9aa50dfb284bd94800c52c4877eeaa5
Chris <yg012014@mymail.pomona.edu>
2018-02-17 05:11:57 -0800

    Add email functionality

--- aspc/forum/views.py
+++ aspc/forum/views.py
@@ -6,6 +6,8 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from aspc.forum.forms import PostForm, QuestionForm, AnswerForm, SearchForm
 from aspc.forum.models import Question, Post, Answer
+from django.core.mail import send_mail
+from aspc.settings import EMAIL_HOST_USER
 
 class PostView(View):
     @method_decorator(login_required)
@@ -24,7 +26,6 @@ class PostView(View):
         else:
             return render(request, 'posts/new_post.html', {'form' : form})
         
-
 class QuestionView(View):
     @method_decorator(login_required)
     def get(self, request):
@@ -39,6 +40,7 @@ class QuestionView(View):
             question.save()
             form.save_m2m()
         return redirect(reverse('all_questions'))
+
 
 class AnswerView(View):
     @method_decorator(login_required)
@@ -55,17 +57,23 @@ class AnswerView(View):
             answer.question = question
             answer.save()
             form.save_m2m()
-        return redirect(reverse('all_questions'))
-   
+            subject = 'You\'ve got a new answer to your question!'
+            content = 'You\'ve received a new answer to your question! Check at https://aspc.pomona.edu/forum/question/{0}'.format(question_id)
+            send_mail(subject,content,EMAIL_HOST_USER,[question.author.email])
+        return redirect(reverse('question', kwargs={'question_id': question_id}))
 
+
+@login_required
 def showAllPosts(request):
     posts = Post.objects.all()
     return render(request, 'posts/posts.html', {'posts' : posts})
 
+@login_required
 def showAllQuestions(request):
     questions = Question.objects.all()
     return render(request, 'qna/questions.html', {'questions': questions})
 
+@login_required
 def question(request, question_id):
     question = get_object_or_404(Question, id=question_id)
     return render(request, 'qna/question.html', {'q': question})
@@ -75,7 +83,7 @@ def post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'posts/post.html', {'p': post})
 
-
+@login_required
 def home(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
@@ -94,6 +102,7 @@ def home(request):
         return render(request, 'general/forum_home.html', {'form': form})
     return render(request, 'general/forum_home.html', {'form': form})
 
+@login_required
 def showResources(request):
     return render(request, 'general/resources.html')
 
